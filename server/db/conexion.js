c00f3f03838320fc6c1ad736f07dbe227c5b8dfe
felipe-pm <pipeeen@gmail.com>
2023-10-18 16:14:57 -0300

    Se sube la conexion editada 2

--- server/db/conexion.js
+++ server/db/conexion.js
@@ -10,8 +10,7 @@ require('dotenv').config();
 }) */
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: true
+    connectionString: process.env.DATABASE_URL
 })
 
 module.exports = pool
